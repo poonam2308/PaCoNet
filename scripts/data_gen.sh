8858dcd1615ff155feba7f474391c4c53707a7bc
poonam2308 <poonamps1791@gmail.com>
2025-08-07 17:29:21 +0200

    test

--- scripts/data_gen.sh
+++ scripts/data_gen.sh
@@ -1,5 +1,5 @@
 # to generate synthetic data
-python pc/plot_gen/plots_processing.py --cfg config/train_config.yaml --task run_dist
+python pc/plot_gen/plots_processing.py --cfg pc/config/train_config.yaml --task run_dist
 
 # to generate plots from synthetic data(csvs)
 #python plots_processing.py --cfg configs/train_config.yaml --task run
