2279ebc3398ab2aa1ff17d84e98aaa2dd69e4232
poonam2308 <poonamps1791@gmail.com>
2025-08-19 09:14:31 +0200

    test_1k

--- scripts/pc.sh
+++
@@ -1,6 +0,0 @@
-#python plots_processing.py --cfg configs/train_config.yaml
-
-#python plots_processing.py --cfg configs/train_config.yaml --task generate_data
-
-python pc/plot_gen/plots_processing.py --cfg pc/config/test_config.yaml --num_files 1000 --task run_dist
-python pc/plot_gen/plots_processing.py --cfg pc/config/test_config.yaml --num_files 1000 --task run
