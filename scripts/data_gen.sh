92a5ec2596cbc4c19a2d2686215d29a174da8d33
poonam2308 <poonamps1791@gmail.com>
2025-08-20 12:28:33 +0200

    tel

--- scripts/data_gen.sh
+++ scripts/data_gen.sh
@@ -1,4 +1,4 @@
-python pc/plot_gen/plots_processing.py --cfg pc/config/train_config.yaml --num_files 5000 --task run
+python pc/plot_gen/plots_processing.py --cfg pc/config/train_config.yaml --task run
 
 #python pc/plot_gen/plots_processing.py --cfg pc/config/test_config.yaml --num_files 1000 --task run_dist
-python pc/plot_gen/plots_processing.py --cfg pc/config/test_config.yaml --num_files 1000 --task run
+python pc/plot_gen/plots_processing.py --cfg pc/config/test_config.yaml --task run
