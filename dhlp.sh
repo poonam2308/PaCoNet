80b234dfa8fb26b5f45407eda491545ff50b4984
poonam2308 <poonamps1791@gmail.com>
2025-09-01 17:08:04 +0200

    generate dhlp data

--- dhlp.sh
+++ dhlp.sh
@@ -1,1 +1,6 @@
-python train.py --identifier baseline config/clust5kdenoisednew.yaml
+# Step 1 generate the npz for each image using the category separated images and the lines json present in train and valid json
+./src/dhlp/dataset/wireframe_noisy.py data/synthetic_plots/multi_cat/training/color data/dhlp/pcw_1n
+
+
+
+#python train.py --identifier baseline config/clust5kdenoisednew.yaml
