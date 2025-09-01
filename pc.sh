b8c5a6c74e55834d68c3a382ccd0ecc8fc003380
poonam2308 <poonamps1791@gmail.com>
2025-09-01 17:17:05 +0200

    requirements updated

--- pc.sh
+++ pc.sh
@@ -1,7 +1,9 @@
 # Create raw csv files and synthetic multi category plots for training and testing set ( 5000 and 1000)
+# training
 #python src/pc/plot_gen/plots_processing.py --cfg src/pc/config/train_config.yaml --num_files 5000 --task run_dis
-# python src/pc/plot_gen/plots_processing.py --cfg src/pc/config/train_config.yaml --num_files 5000 --task run
+#python src/pc/plot_gen/plots_processing.py --cfg src/pc/config/train_config.yaml --num_files 5000 --task run
 
+# testing
 #python src/pc/plot_gen/plots_processing.py --cfg src/pc/config/test_config.yaml --num_files 1000 --task run_dist
 #python src/pc/plot_gen/plots_processing.py --cfg src/pc/config/test_config.yaml --num_files 1000 --task run
 
@@ -13,4 +15,11 @@
 
 
 # after separation the category based on colors, also  set train and valid lines coordinates from all_data json
-python src/pc/plot_gen/plots_processing.py --cfg src/pc/config/train_config.yaml --task split_data
+#python src/pc/plot_gen/plots_processing.py --cfg src/pc/config/train_config.yaml --task split_data
+
+
+# unet training with noisy images
+# unet training with non noisy images
+python unet_train.py --cfg configs/train_config.yaml
+
+python unet_inference.py --cfg configs/test_config.yaml
--- unet.sh
+++
@@ -1,3 +0,0 @@
-python unet_train.py --cfg configs/train_config.yaml
-
-python unet_inference.py --cfg configs/test_config.yaml
