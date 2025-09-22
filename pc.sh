d538f36c4b73d94f385620a2bed4e68166c0c994
poonam2308 <poonamps1791@gmail.com>
2025-09-22 10:32:47 +0200

    batch size

--- pc.sh
+++ pc.sh
@@ -27,4 +27,4 @@
 #
 #python unet_inference.py --cfg configs/test_config.yaml
 
-python src/pc/unet_train.py --cfg src/pc/config/train_config.yaml --num_epochs 50
+python src/pc/unet_train.py --cfg src/pc/config/train_config.yaml --batch_size 8 --num_epochs 50
