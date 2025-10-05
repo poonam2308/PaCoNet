2bf4172281e583f147b659aecc77f959fc4a4598
poonam2308 <poonamps1791@gmail.com>
2025-10-05 23:45:29 +0200

    all

--- pc.sh
+++ pc.sh
@@ -25,7 +25,7 @@ python src/pc/plot_gen/plots_processing.py --cfg src/pc/config/test_config.yaml 
 #
 #python unet_inference.py --cfg configs/test_config.yaml
 
-#python src/pc/unet_train.py --cfg src/pc/config/train_config.yaml --batch_size 8 --num_epochs 80
+python src/pc/unet_train.py --cfg src/pc/config/train_config.yaml --batch_size 8 --num_epochs 80
 
 
 
