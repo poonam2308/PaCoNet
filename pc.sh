68b8df4f3b2c5e1caa7ed7cc89eb5fd50ba4d785
poonam2308 <poonamps1791@gmail.com>
2025-10-05 19:31:04 +0200

    testdata

--- pc.sh
+++ pc.sh
@@ -6,18 +6,19 @@
 # create crops and those are saved in a cropping images directory  ( it is performed with run method)
 
 # after cropping perform category separation
-python src/pc/plot_gen/plots_processing.py --cfg src/pc/config/train_config.yaml --task separate_by_color
+#python src/pc/plot_gen/plots_processing.py --cfg src/pc/config/train_config.yaml --task separate_by_color
 
 # after separating the category based on the colors, scale the alllines data because unet will produde 224 size denoised images and those images are used in dhlp
-python src/pc/plot_gen/plots_processing.py --cfg src/pc/config/train_config.yaml --task rescale_lines
+#python src/pc/plot_gen/plots_processing.py --cfg src/pc/config/train_config.yaml --task rescale_lines
 
 # after separating the category based on colors, also  set train and valid lines coordinates from all_data json
-python src/pc/plot_gen/plots_processing.py --cfg src/pc/config/train_config.yaml --task split_data
+#python src/pc/plot_gen/plots_processing.py --cfg src/pc/config/train_config.yaml --task split_data
 
 # testing
 #python src/pc/plot_gen/plots_processing.py --cfg src/pc/config/test_config.yaml --num_files 1000 --seed 0 --task run_dist
 #python src/pc/plot_gen/plots_processing.py --cfg src/pc/config/test_config.yaml --num_files 1000 --seed 0 --task run
-python src/pc/plot_gen/plots_processing.py --cfg src/pc/config/test_config.yaml --seed 0 --task separate_by_color
+#python src/pc/plot_gen/plots_processing.py --cfg src/pc/config/test_config.yaml --seed 0 --task separate_by_color
+
 python src/pc/plot_gen/plots_processing.py --cfg src/pc/config/test_config.yaml --seed 0 --task rescale_lines
 
 
