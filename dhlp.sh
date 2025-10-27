7a9f882bb0917129684b648d78721d183f6c4cd3
poonam2308 <poonamps1791@gmail.com>
2025-10-27 10:23:59 +0100

    DHLP_README

--- dhlp.sh
+++ dhlp.sh
@@ -6,7 +6,22 @@
 # Step 2
 # once the data is present provide the same path in the yaml where data is preset and start the training
 
-python ./src/dhlp/train.py --identifier baseline ./src/dhlp/config/noisy.yaml
+#python ./src/dhlp/train.py --identifier baseline ./src/dhlp/config/noisy.yaml
 
 
 #./process_original.py config/clust5kdenoisednew.yaml logs_clst5kdenew/250224-133604-baseline/checkpoint_best.pth
+
+
+
+
+# this is for the denoised images
+# follow step 1 to create the data
+#  - use denoised.yaml and wireframe_denoised.py module
+# verify the paths are correct
+# step 1
+
+./src/dhlp/dataset/wireframe_denoised.py data/synthetic_plots/multi_cat/training/color/denoised data/dhlp/pcw_2dn
+
+
+# step 2
+#python ./src/dhlp/train.py --identifier baseline ./src/dhlp/config/denoised.yaml
