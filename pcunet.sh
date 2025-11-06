f2c2f6963d21db7e8c8a067d9b9909b062d68d2c
poonam2308 <poonamps1791@gmail.com>
2025-11-06 15:29:17 +0100

    unet_op

--- pcunet.sh
+++ pcunet.sh
@@ -1,9 +1,14 @@
 # training unet
 
-python src/pc/unet_train.py --cfg src/pc/config/train_config.yaml --batch_size 8 --num_epochs 80
+#python src/pc/unet_train.py --cfg src/pc/config/train_config.yaml --batch_size 8 --num_epochs 80
 
 # generating denoised images using trained unet
 
 #python src/pc/unet_inference.py --cfg src/pc/config/test_config.yaml --batch_size 8
+
+
+# for the opaque pc images
+
+python src/pc/unet_train.py --cfg src/pc/config/train_config_op.yaml --batch_size 8 --num_epochs 80
 
 
