6cba06bde11cfe029337e236daac7c3367d55b17
poonam2308 <poonamps1791@gmail.com>
2025-11-11 15:40:09 +0100

    ELBO_full

--- pcdata.sh
+++ pcdata.sh
@@ -22,16 +22,16 @@
 
 #separating the categories based on cluster elbo bayesian gmm  reduced size
 
-python src/pc/plot_gen/plots_processing.py --cfg src/pc/config/train_config.yaml --task separate_by_elbo
-python src/pc/plot_gen/plots_processing.py --cfg src/pc/config/train_config.yaml --task rescale_lines_elbo
-python src/pc/plot_gen/plots_processing.py --cfg src/pc/config/train_config.yaml --task split_data_elbo
+#python src/pc/plot_gen/plots_processing.py --cfg src/pc/config/train_config.yaml --task separate_by_elbo
+#python src/pc/plot_gen/plots_processing.py --cfg src/pc/config/train_config.yaml --task rescale_lines_elbo
+#python src/pc/plot_gen/plots_processing.py --cfg src/pc/config/train_config.yaml --task split_data_elbo
 
 
 #separating the categories based on cluster elbo bayesian gmm  full size
 
-#python src/pc/plot_gen/plots_processing.py --cfg src/pc/config/train_config.yaml --task separate_by_elbo_fres
-#python src/pc/plot_gen/plots_processing.py --cfg src/pc/config/train_config.yaml --task rescale_lines_elbo_fres
-#python src/pc/plot_gen/plots_processing.py --cfg src/pc/config/train_config.yaml --task split_data_elbo_fres
+python src/pc/plot_gen/plots_processing.py --cfg src/pc/config/train_config.yaml --task separate_by_elbo_fres
+python src/pc/plot_gen/plots_processing.py --cfg src/pc/config/train_config.yaml --task rescale_lines_elbo_fres
+python src/pc/plot_gen/plots_processing.py --cfg src/pc/config/train_config.yaml --task split_data_elbo_fres
 
 
 # category evaluation
