0707dd016a3ef90d4bd1b81bfa404298699cfd27
poonam2308 <poonamps1791@gmail.com>
2025-12-02 23:22:25 +0100

    ncolor

--- dhlp.sh
+++ dhlp.sh
@@ -58,13 +58,13 @@
 #./src/dhlp/dataset/wireframe_noised.py data/synthetic_plots/multi_cat/training/color  data/dhlp/pcw_ncolor
 
 # step 2
-#python ./src/dhlp/train.py --identifier baseline ./src/dhlp/config/noisedPeak.yaml
+python ./src/dhlp/train.py --identifier baseline ./src/dhlp/config/noisedPeak.yaml
 
 
 # create dhlp format dataset for without unet dbscan peak data
 
 # step 1
-./src/dhlp/dataset/wireframe_noised.py data/synthetic_plots/multi_cat/training/cluster  data/dhlp/pcw_ncluster
+#./src/dhlp/dataset/wireframe_noised.py data/synthetic_plots/multi_cat/training/cluster  data/dhlp/pcw_ncluster
 
 # step 2
 #python ./src/dhlp/train.py --identifier baseline ./src/dhlp/config/noisedCluster.yaml
