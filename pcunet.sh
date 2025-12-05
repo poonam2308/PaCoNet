8a7153a4eb5f81436f393c304ca603f7ce8c182c
poonam2308 <poonamps1791@gmail.com>
2025-12-05 09:49:17 +0100

    unet_test_cls

--- pcunet.sh
+++ pcunet.sh
@@ -9,7 +9,7 @@
 #python src/pc/unet_inference.py --cfg src/pc/config/train_config.yaml --batch_size 8
 
 #for test dataset - generating the denoised images from the test dataset for color based unet
-#python src/pc/unet_inference.py --cfg src/pc/config/test_config.yaml --batch_size 8
+python src/pc/unet_inference.py --cfg src/pc/config/test_config.yaml --batch_size 8
 
 
 # for the opaque pc images
@@ -22,4 +22,4 @@
 
 #python src/pc/unet_inference.py --cfg src/pc/config/test_config_op.yaml --batch_size 8
 
-python src/pc/unet_inference.py --cfg src/pc/config/test_config_op.yaml --batch_size 8
+#python src/pc/unet_inference.py --cfg src/pc/config/test_config_op.yaml --batch_size 8
