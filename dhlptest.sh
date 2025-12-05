eb3420fdeecfac074de5257449ac765106f31912
poonam2308 <poonamps1791@gmail.com>
2025-12-05 14:21:33 +0100

    pcs_test

--- dhlptest.sh
+++ dhlptest.sh
@@ -84,11 +84,11 @@
 # output :generate the npz for each image
 # input : using the category separated images and the lines json present in  test.json
 
-./src/dhlp/dataset/wireframe_test.py data/synthetic_plots/multi_cat/testing/cluster data/dhlp/pcw_ntest_cls
+#./src/dhlp/dataset/wireframe_test.py data/synthetic_plots/multi_cat/testing/cluster data/dhlp/pcw_ntest_cls
 
 # Step 2
 # create masks for test data
-./src/dhlp//dataset/gen_mask.py data/dhlp/pcw_ntest_cls/test data/dhlp/pcw_ntest_cls/masks
+#./src/dhlp//dataset/gen_mask.py data/dhlp/pcw_ntest_cls/test data/dhlp/pcw_ntest_cls/masks
 
 
 #%% ------------ denoised unet cluster  ----------
@@ -96,8 +96,8 @@
 # output :generate the npz for each image
 # input : using the category separated images and the lines json present in  test.json
 
-#./src/dhlp/dataset/wireframe_test.py data/synthetic_plots/multi_cat/testing/cluster data/dhlp/pcw_test_cls
+./src/dhlp/dataset/wireframe_test.py data/synthetic_plots/multi_cat/testing/cluster data/dhlp/pcw_test_cls
 
 # Step 2
 # create masks for test data
-#./src/dhlp//dataset/gen_mask.py data/dhlp/pcw_test_cls/test data/dhlp/pcw_test_cls/masks
+./src/dhlp//dataset/gen_mask.py data/dhlp/pcw_test_cls/test data/dhlp/pcw_test_cls/masks
