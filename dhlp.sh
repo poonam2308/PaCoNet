90027e19ee8fc71525b67f2e809495044618ddc7
poonam2308 <poonamps1791@gmail.com>
2025-12-10 15:23:55 +0100

    crops_dhlp

--- dhlp.sh
+++ dhlp.sh
@@ -1,8 +1,16 @@
+# step 1
+# generate np for each image using crops_white images and the lines present in train and valid json
+./src/dhlp/dataset/wireframe_all_test.py data/synthetic_plots/multi_cat/training/color data/dhlp/pcw_crops
+
+# Step 2
+# once the data is present provide the same path in the yaml where data is preset and start the training
+#python ./src/dhlp/train.py --identifier baseline ./src/dhlp/config/crops.yaml
+
+
 # Step 1
 # generate the npz for each image using the category separated images and the lines json present in train and valid json
 #
 #./src/dhlp/dataset/wireframe_noisy.py data/synthetic_plots/multi_cat/training/color data/dhlp/pcw_1n
-
 
 # Step 2
 # once the data is present provide the same path in the yaml where data is preset and start the training
